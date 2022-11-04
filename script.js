0d83dad7e21295e04db94e479e775e81cb36906f
Ahmed Modather <115614683+Ahmed-Modather@users.noreply.github.com>
2022-11-04 15:06:48 +0200

    Add function plays a single round of game
    Note/two typos are fixed in the previous commited functions - it refered in code as //(*)

--- script.js
+++ script.js
@@ -3,23 +3,56 @@ function getComputerChoice() {
     let ranNum = Math.floor(Math.random() * 3) + 1; //random 
 
     if (ranNum===1) {
-        return 'Rock';
+        return 'rock';     //(*)first charachters edited from capital to small
     } else if (ranNum===2 ) {
-        return 'Paper';
+        return 'paper';
     } else {
-      return 'Scissors';  
+      return 'scissors';  
     }
     
 }
 
+//Second: function ask user to enter ‘Rock’ or ‘Paper’ or ‘Scissors’ and certain
+// the user will enter proper value.
 function getUserChoice() {
     let choice = prompt("Enter 'Rock' or 'Paper' or 'Scissors'");
     choice = choice.toLowerCase();
-    console.log(choice)
 
-    while (choice !=="rock" && choice!=="paper" && choice!=="Scissors") {
+    while (choice !=="rock" && choice!=="paper" && choice!=="scissors") {   //(*)Scissors=>scissors
         choice = prompt("Please, enter valid value! [Rock/Paper/Scissors]")
         choice = choice.toLowerCase();
     }
     return choice;
 }
+
+//Third: function take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round
+function playRound(playerSelection, computerSelection) {
+    const player = playerSelection();
+    const computer = computerSelection();
+    
+    if (player===computer) {
+        let x = `The score is tied!
+    Your play: ${player} 
+    Computer play: ${computer}`;
+        return x;
+
+    } else if ((player==="rock"&&computer==="scissors")||(player==="scissors"&&computer==="paper")||(player==="paper"&&computer==="rock")) {
+        let x= `Congratulations, You Won!
+        Your play: ${player} 
+        Computer play: ${computer}
+        * ${player} beats ${computer}`;
+        return x;
+
+    } else {
+        let x = `Unfortunately, You Lose!
+        Your play: ${player} 
+        Computer play: ${computer}
+        * ${computer} beats ${player}`;
+        return x;
+    } 
+}    
+
+ 
+
+
+
